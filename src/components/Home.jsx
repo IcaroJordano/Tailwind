6d0cb84b13e9e0bcd1145ef01b548e38e3e1aa97
Icaro Jordano <Icarojordano>
2024-10-14 06:20:38 -0300

    feat: h1 desktop and mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 export function Home() {
     return (
         <section className=" h-3/6 ">
-            <h1 className=" ml-24 mt-28  text-5xl w-3/6 text-white" >Software designer, founder, and amateur astronaut.</h1>
+            <h1 className=" ml-4 lg:ml-24 mt-28 text-3xl lg:text-5xl lg:font-bold lg:leading-tight w-11/12 lg:w-3/6  text-white" >Software designer, founder, and amateur astronaut.</h1>
             <p className="ml-24 mt-9 w-6/12 text-slate-400 text-base tracking-widest">I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.</p>
             <div className="w-2/12 ml-24 mt-5 h-6 flex  justify-between">
                 <div className="bg-slate-400 w-1/6 rounded-lg " >.</div>
