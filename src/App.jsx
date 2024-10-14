765b3bfe57a2d628402c62f21ceeac30fc0d9e02
Icaro Jordano <Icarojordano>
2024-10-14 07:19:53 -0300

    feat: images mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header"
 import { Home } from "./components/Home"
+import { Images } from "./components/Images"
 
 function App() {
 
   return (
-    <div className="bg-zinc-950 w-dvh flex justify-center h-screen">
-      <div className="bg-zinc-900  h-lvh w-full lg:w-11/12 ">
+    <div className="bg-zinc-950 w-dvh flex justify-center overflow-hidden">
+      <div className="bg-zinc-900 overflow-hidden h-full w-full lg:w-11/12 ">
         <Header></Header>
         <Home></Home>
+        <Images></Images>
       </div>
     </div>
   )
---
+++ src/components/Images.jsx
@@ -0,0 +1,9 @@
+export function Images() {
+    return(
+        <section className="h-56 flex w-screen overflow-hidden mt-16"  >
+            <div className="bg-white rounded-md  w-5/12 h-48 mx-auto -rotate-2">.</div>
+            <div className="bg-white rounded-md mx-5 w-10/12 h-48  rotate-2">.</div>
+            <div className="bg-white rounded-md w-5/12 h-48 mx-auto rotate-3">.</div>
+        </section>
+    )
+}
