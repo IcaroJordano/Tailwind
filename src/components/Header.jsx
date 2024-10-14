dcac14b50bd93125795819708609e0114002a6e2
Icaro Jordano <Icarojordano>
2024-10-14 05:43:56 -0300

    feat:create component Home desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
     return (
         <header className="w-full h-11  my-6  align-baseline justify-between flex ">
             <div className="w-10 h-10   bg-slate-50  ml-28 rounded-full"> .</div>
-            <nav className="w-24 align-bottom justify-around flex bg-zinc-800 rounded-full sm:w-4/12 " >
+            <nav className="w-96 align-bottom justify-around flex bg-zinc-800 rounded-full" >
                 <a className="text-slate-100 text-sm text-center my-auto " href="">About</a>
                 <a className="text-slate-100 text-sm text-center my-auto " href="">Artices</a>
                 <a className="text-slate-100 text-sm text-center my-auto " href="">Projects</a>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 export function Home() {
-    return(
-        <section className="bg-white h-3/5">
-            /
+    return (
+        <section className=" h-3/6 ">
+            <h1 className=" ml-24 mt-28  text-5xl w-3/6 text-white" >Software designer, founder, and amateur astronaut.</h1>
+            <p className="ml-24 mt-9 w-6/12 text-slate-400 text-base tracking-widest">I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.</p>
+            <div className="w-2/12 ml-24 mt-5 h-6 flex  justify-between">
+                <div className="bg-slate-400 w-1/6 rounded-lg " >.</div>
+                <div className="bg-slate-400 w-1/6 rounded-lg" >.</div>
+                <div className="bg-slate-400 w-1/6 rounded-lg" >.</div>
+                <div className="bg-slate-400 w-1/6 rounded-lg" >.</div>
+            </div>
         </section>
     )
 }
