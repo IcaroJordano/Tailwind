a7f2b627ce5f86f11ea20741f75662c0d3dbd86a
Icaro Jordano <Icarojordano>
2024-10-17 02:42:56 -0300

    feat(Scroll): cria uma class tailwind de estilo temporario para o scoll da pagina

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Images } from "./components/Images"
 function App() {
 
   return (
-    <div className="bg-zinc-950 w-dvh flex justify-center  overflow-hidden">
+    <div className="bg-zinc-950 custom-scrollbar w-dvh flex justify-center  overflow-hidden">
       <div className="bg-zinc-900 lg:overflow-visible overflow-hidden h-full w-auto lg:w-11/12 ">
         <Header></Header>
         <Home></Home>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ export function Header() {
         return () => window.removeEventListener('scroll', handleScroll); // Limpa o eventListener
     }, [lastScrollTop]);
     return (
-        <header className={`w-full h-11 z-50 transition-transform duration-150   fixed    align-baseline justify-between flex ${isHidden ? "-translate-y-full" : 'translate-y-5'}`}>
+        <header className={`w-full h-11 z-50 transition-transform duration-300   fixed    align-baseline justify-between flex ${isHidden ? "-translate-y-full" : 'translate-y-5'}`}>
             <div className="w-10 h-10   bg-zinc-800 ml-4 lg:ml-28 rounded-full"> .</div>
             <NavDesktop></NavDesktop>
             <NavMobile></NavMobile>
