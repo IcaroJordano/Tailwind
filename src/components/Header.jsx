c45576a60114f8f6a0dc9796992ca6583cba1406
Icaro Jordano <Icarojordano>
2024-10-17 02:28:46 -0300

    feat(Header): add efeito Auto-Hidding Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import { NavDesktop } from "./NavDesktop";
 import { NavMobile } from "./NavMobile";
-
+import { useEffect, useState } from "react"
 
 export function Header() {
+    const [lastScrollTop, setLastScrollTop] = useState(0);
+    const [isHidden, setIsHidden] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+            if (currentScroll > lastScrollTop) {
+                setIsHidden(true); // Esconde o menu ao rolar para baixo
+            } else {
+                setIsHidden(false); // Mostra o menu ao rolar para cima
+            }
+            setLastScrollTop(currentScroll);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll); // Limpa o eventListener
+    }, [lastScrollTop]);
     return (
-        <header className="w-full h-11  my-6  align-baseline justify-between flex ">
-            <div className="w-10 h-10   bg-slate-50 ml-4 lg:ml-28 rounded-full"> .</div>
+        <header className={`w-full h-11 z-50 transition-transform duration-150   fixed    align-baseline justify-between flex ${isHidden ? "-translate-y-full" : 'translate-y-5'}`}>
+            <div className="w-10 h-10   bg-zinc-800 ml-4 lg:ml-28 rounded-full"> .</div>
             <NavDesktop></NavDesktop>
             <NavMobile></NavMobile>
-            <div className="w-12 lg:w-14 items-center justify-center flex bg-gray-500/10 rounded-3xl lg:mr-28 border border-neutral-600/20 mr-3" >.</div>
+            <div className="w-12 lg:w-14 items-center justify-center flex bg-zinc-800 rounded-3xl lg:mr-56 border border-neutral-600/20 mr-3" >.</div>
         </header>
     )
 }
--- src/components/NavDesktop.jsx
+++ src/components/NavDesktop.jsx
@@ -1,6 +1,8 @@
+
 export function NavDesktop() {
+
     return(
-        <nav className="w-96 hidden lg:flex align-bottom justify-around bg-zinc-800 rounded-full" >
+        <nav className={`w-96 hidden lg:flex align-bottom justify-around bg-zinc-800 rounded-full `} >
                 <a className="text-slate-100 text-sm text-center my-auto " href="">About</a>
                 <a className="text-slate-100 text-sm text-center my-auto " href="">Artices</a>
                 <a className="text-slate-100 text-sm text-center my-auto " href="">Projects</a>
