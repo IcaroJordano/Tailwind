2335e9bbeb7e50a1332de0aa353a1ebc5bdd0831
Icaro Jordano <Icarojordano>
2024-10-18 02:29:31 -0300

    fix(image): adapta o margin left na versao dektop

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,6 +1,6 @@
 export function Images() {
     return(
-        <section className="flex  relative w-fit lg:w-fit overflow-x-hidden overflow-hidden lg:overflow-visible -ml-16 lg:-ml-40 mt-16 "  >
+        <section className="flex  relative w-fit lg:w-fit overflow-x-hidden overflow-hidden lg:overflow-visible -ml-16 lg:-ml-28 mt-16 "  >
             <img alt="" src="https://spotlight.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&w=384&q=75" className="bg-white dark:bg-neutral-600 rounded-2xl rotate-2  w-5/12  lg:w-72 h-48 lg:h-80 mx-auto lg:mx-4 hidden lg:block" />
             <img alt="" src="https://spotlight.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&w=384&q=75" className="bg-white dark:bg-neutral-600 rounded-2xl -rotate-2 w-40  lg:w-72 h-48 lg:h-80 mx-auto lg:mx-4 " />
             <img alt="" src="https://spotlight.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.5c6d0ed6.jpg&w=384&q=75" className="bg-white dark:bg-neutral-600 rounded-2xl rotate-2  w-40  lg:w-72 h-48 lg:h-80 mx-5 lg:mx-4" />
